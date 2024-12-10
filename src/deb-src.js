29d24a171db7f12328f2a5bbd9676e3e004f5532
Yinan Qin <c.elysia@foxmail.com>
2024-12-10 14:18:14 +0000

    update

--- src/deb-src.js
+++ src/deb-src.js
@@ -7,19 +7,17 @@ async function build_deb_src() {
 
     await exec.exec('sudo apt install -y devscripts equivs')
 
-    await exec.exec('dpkg-buildpackage -d -S -us -uc')
+    await exec.exec('debuild --preserve-env --no-lintian -d -S -us -uc')
 
-    await exec.exec(
-      'rm -rf ./debian-src-tarball && mkdir -p ./debian-src-tarball'
-    )
+    await exec.exec('rm -rf ./debian-src-tarball')
+    await exec.exec('mkdir -p ./debian-src-tarball')
 
     await exec.exec('mv -f ../*.dsc ./debian-src-tarball')
     await exec.exec('mv -f ../*.changes ./debian-src-tarball')
     await exec.exec('mv -f ../*.tar.* ./debian-src-tarball')
 
-    await exec.exec(
-      'realpath ./debian-src-tarball && ls -l ./debian-src-tarball'
-    )
+    await exec.exec('realpath ./debian-src-tarball')
+    await exec.exec('ls -l ./debian-src-tarbal')
   } catch (error) {
     // 如果发生错误，使工作流运行失败
     core.setFailed(error.message)
